4bdb5056c94363f00e2ae1a542ff4eb7be8d8ee7
Alexander Fomin <firstmixlucker@gmail.com>
2020-04-16 21:25:04 +0300

    done lesson 56

--- src/components/Page/Page_Items/Page_Items_Users/Container.js
+++ src/components/Page/Page_Items/Page_Items_Users/Container.js
@@ -1,6 +1,56 @@
-import Page_Items_Users from ".";
+import React from 'react'
+import * as axios from 'axios'
 import { connect } from "react-redux"
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from "../../../../reducers/reducer_users";
+import PageItemsUsers from '.'
+
+class UsersContainer extends React.Component {
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    // if (this.props.users.length === 0) {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
+      withCredentials: true,
+      headers: {
+        // 'API-KEY': '2770717b-7856-4262-9cce-04c5522365f3'
+      }
+    })
+      .then(response => {
+        console.log('--- response.data', response.data)
+        this.props.setUsers(response.data.items)
+        this.props.setTotalUsersCount(response.data.totalCount)
+      });
+    // }
+  }
+  // componentWillUnmount() {
+  //   console.log('componentWillUnmount');
+  // }
+
+  handleClick = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
+      withCredentials: true,
+      headers: {
+        // 'API-KEY': '2770717b-7856-4262-9cce-04c5522365f3'
+      }
+    })
+      .then(response => {
+        console.log('--- response.data', response.data)
+        this.props.setUsers(response.data.items)
+      });
+  }
+
+  render() {
+    return <PageItemsUsers
+      totalUsersCount={this.props.totalUsersCount}
+      pageSize={this.props.pageSize}
+      currentPage={this.props.currentPage}
+      users={this.props.users}
+      unfollow={this.props.unfollow}
+      follow={this.props.follow}
+      handleClick={this.handleClick} />
+  }
+}
 
 const mapStateToProps = (state) => ({
   users: state.usersPage.users,
@@ -15,5 +65,6 @@ const mapDispatchToProps = (dispatch) => ({
   setCurrentPage: (pageNumber) => { dispatch(setCurrentPageAC(pageNumber)) },
   setTotalUsersCount: (totalCount) => { dispatch(setTotalUsersCountAC(totalCount)) }
 })
-export default connect(mapStateToProps, mapDispatchToProps)(Page_Items_Users);
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
 
--- src/components/Page/Page_Items/Page_Items_Users/index.js
+++ src/components/Page/Page_Items/Page_Items_Users/index.js
@@ -1,79 +1,43 @@
-import * as axios from 'axios'
 import React from 'react'
 import './style.scss'
 const S = 'Page_Items_Users'
 
-class Page_Items_Users extends React.Component {
+const Page_Items_Users = (props) => {
 
-  componentDidMount() {
-    console.log('componentDidMount');
-    // if (this.props.users.length === 0) {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
-      withCredentials: true,
-      headers: {
-        // 'API-KEY': '2770717b-7856-4262-9cce-04c5522365f3'
-      }
-    })
-      .then(response => {
-        console.log('--- response.data', response.data)
-        this.props.setUsers(response.data.items)
-        this.props.setTotalUsersCount(response.data.totalCount)
-      });
-    // }
-  }
-  // componentWillUnmount() {
-  //   console.log('componentWillUnmount');
-  // }
-
-  handleClick = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
-      withCredentials: true,
-      headers: {
-        // 'API-KEY': '2770717b-7856-4262-9cce-04c5522365f3'
-      }
-    })
-      .then(response => {
-        console.log('--- response.data', response.data)
-        this.props.setUsers(response.data.items)
-      });
-  }
-
-  render() {
-
-    const createNumbers = () => {
-      const numbers = []
-      const pages = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-      for (let i = 1; i <= pages; i++) {
-        numbers.push(
-          <span onClick={() => this.handleClick(i)} className={
-            this.props.currentPage === i ? `${S}__lists__item active` : `${S}__lists__item`
-          }>{i}</span>
-        )
-      }
-      return numbers
+  const createNumbers = () => {
+    const numbers = []
+    const pages = Math.ceil(props.totalUsersCount / props.pageSize)
+    for (let i = 1; i <= pages; i++) {
+      numbers.push(
+        <span key={i} onClick={() => props.handleClick(i)} className={
+          props.currentPage === i ? `${S}__lists__item active` : `${S}__lists__item`
+        }>{i}</span>
+      )
     }
+    return numbers
+  }
 
-    return (
-      <div className={`${S}`}>
-        <div className={`${S}__lists`}>
-          {createNumbers()}
-        </div>
-        {
-          this.props.users.map(user => <div key={user.id}>
+  return (
+    <div className={`${S}`}>
+      <div className={`${S}__lists`}>
+        {createNumbers()}
+      </div>
+      {
+        props.users.map(user =>
+          <div key={user.id}>
             <div className={`${S}__wrapper`}>
-              <div className={`${S}__face`}>
+              <div className={`${S}__face`} >
                 <img src={
                   user.photos.small === null
                     ? `https://placem.at/people?w=90&h=90&random=${user.id}`
                     : user.photos.small
                 } alt="" />
                 {user.followed
-                  ? <button onClick={() => this.props.unfollow(user.id)}>unfollow</button>
-                  : <button onClick={() => this.props.follow(user.id)}>follow</button>
+                  ? <button onClick={() => props.unfollow(user.id)}>unfollow</button>
+                  : <button onClick={() => props.follow(user.id)}>follow</button>
                 }
               </div>
-              <div className={`${S}__about`}>
+              <div className={`${S}__about`} >
                 <div className={`${S}__about__fullname`}>
                   {user.name}
                 </div>
@@ -87,11 +51,10 @@ class Page_Items_Users extends React.Component {
               </div>
             </div>
           </div>)
-        }
-        <button>SHOW_MORE</button>
-      </div>
-    )
-  }
+      }
+      <button>SHOW_MORE</button>
+    </div>
+  )
 }
 
 export default Page_Items_Users
